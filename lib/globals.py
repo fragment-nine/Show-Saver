b694446d653a9f3f749e41b78faad095150c351b
andrewgange <andrewmgange@gmail.com>
2024-07-19 23:20:29 -0500

    revert back to original audio routing - further UI look development
    with further testing this evening, I noticed my setup of audio routing was nearly impossible to manipulate in a clean way from the UI. reverting back to what we know works and continuing to push this. got fancy sliders and some other elements looking good which I am excited about.

--- lib/globals.py
+++ lib/globals.py
@@ -1,5 +1,5 @@
 v1l	test
-v1d	V1|||0|||0|||0|||OBS Virtual Camera
+v1d	V1|||\\?\usb#vid_12d1&pid_4321&mi_00#9&1350f74c&2&0000#{65e8773d-8f56-11d0-a3b9-00a0c9223196}\global|||0|||0|||j5 WebCam JVCU100
 a1l	default
 a1d	AppleUSBAudioEngine:ZOOM Corporation:AMS-24:3200000:1,2
 a2l	default
@@ -8,4 +8,4 @@ a3l	default
 a3d	BF568F24-731B-41DB-932E-AC7E260BC71A
 postRoll	15
 outputFolder	/Users/jacksongallagher/Documents/Show-Saver/test
-longPrefix	GreenDay24_
+longPrefix	TestGANGE_
--- lib/project1/Embody/externalizations.py
+++ lib/project1/Embody/externalizations.py
@@ -1,14 +1,13 @@
 network_path	type	rel_file_path	timestamp	dirty
-/project1/ui	container	lib/project1/ui.tox	2024-07-09 15:43:59	False
-/project1/deko	container	lib/project1/deko.tox	2024-07-09 15:43:59	False
+/project1/deko	container	lib/project1/deko.tox	2024-07-19 23:14:25	False
 /project1/files_out	container	lib/project1/files_out.tox	2024-07-09 15:43:59	False
-/project1/start_stop	container	lib/project1/start_stop.tox	2024-07-09 15:43:59	False
-/project1/video_input	container	lib/project1/video_input.tox	2024-07-09 15:43:59	False
+/project1/start_stop	container	lib/project1/start_stop.tox	2024-07-19 23:14:25	False
+/project1/video_input	container	lib/project1/video_input.tox	2024-07-19 23:14:25	False
 /project1/track_master	container	lib/project1/track_master.tox	2024-07-09 15:43:59	False
 /project1/Embody/externalizations	table	lib/project1/Embody/externalizations.py	2023-12-29 14:57:55	
 /project1/start_stop/startLTC	chopexec	lib/project1/start_stop/startLTC.py	2023-12-29 14:58:39	
 /globals	table	lib/globals.py	2023-12-29 15:01:36	
-/project1/ui/v_select/vD_Execute	datexec	lib/project1/ui/v_select/vD_Execute.py	2023-12-29 15:01:36	
+/SS_UI_v2/UI_Main/left_data/1_input_select/input_select/v_select/vD_Execute	datexec	lib/SS_UI_v2/UI_Main/left_data/1_input_select/input_select/v_select/vD_Execute.py	2023-12-29 15:01:36	
 /project1/ui/v_select/vL_Execute	chopexec	lib/project1/ui/v_select/vL_Execute.py	2023-12-29 15:01:36	
 /project1/start_stop/tools	execute	lib/project1/start_stop/tools.py	2023-12-29 15:01:36	
 /project1/start_stop/trigExecute	chopexec	lib/project1/start_stop/trigExecute.py	2023-12-29 15:01:36	
@@ -18,9 +17,27 @@ network_path	type	rel_file_path	timestamp	dirty
 /device_data/text1	text	lib/device_data/text1.py	2023-12-29 15:01:36	
 /device_data/text2	text	lib/device_data/text2.py	2023-12-29 15:01:36	
 /device_data/refresh_av_info	chopexec	lib/device_data/refresh_av_info.py	2023-12-29 15:01:36	
-/project1/ui/v_select/vD_Execute	datexec	lib/project1/ui/v_select/vD_Execute.py	2024-07-17 12:23:34	
+/pages_main/pgm/pgm_info/device_select/v_select/vD_Execute	datexec	lib/pages_main/pgm/pgm_info/device_select/v_select/vD_Execute.py	2024-07-17 12:23:34	
 /project1/ui/v_select/vL_Execute	chopexec	lib/project1/ui/v_select/vL_Execute.py	2024-07-17 12:23:34	
 /project1/start_stop/tools	execute	lib/project1/start_stop/tools.py	2024-07-17 12:23:34	
 /project1/start_stop/startLTC	chopexec	lib/project1/start_stop/startLTC.py	2024-07-17 12:23:34	
 /project1/start_stop/trigExecute	chopexec	lib/project1/start_stop/trigExecute.py	2024-07-17 12:23:34	
 /project1/start_stop/manual_start_exec	chopexec	lib/project1/start_stop/manual_start_exec.py	2024-07-17 12:23:34	
+/project1/ui/v_select/vD_Execute	datexec	lib/project1/ui/v_select/vD_Execute.py	2024-07-18 11:31:10	
+/project1/ui/v_select/vL_Execute	chopexec	lib/project1/ui/v_select/vL_Execute.py	2024-07-18 11:31:10	
+/project1/start_stop/tools	execute	lib/project1/start_stop/tools.py	2024-07-18 11:31:10	
+/project1/start_stop/startLTC	chopexec	lib/project1/start_stop/startLTC.py	2024-07-18 11:31:10	
+/project1/start_stop/trigExecute	chopexec	lib/project1/start_stop/trigExecute.py	2024-07-18 11:31:10	
+/project1/start_stop/manual_start_exec	chopexec	lib/project1/start_stop/manual_start_exec.py	2024-07-18 11:31:10	
+/project1/ui/v_select/vD_Execute	datexec	lib/project1/ui/v_select/vD_Execute.py	2024-07-19 15:58:50	
+/project1/ui/v_select/vL_Execute	chopexec	lib/project1/ui/v_select/vL_Execute.py	2024-07-19 15:58:50	
+/project1/start_stop/tools	execute	lib/project1/start_stop/tools.py	2024-07-19 15:58:50	
+/project1/start_stop/startLTC	chopexec	lib/project1/start_stop/startLTC.py	2024-07-19 15:58:51	
+/project1/start_stop/trigExecute	chopexec	lib/project1/start_stop/trigExecute.py	2024-07-19 15:58:51	
+/project1/start_stop/manual_start_exec	chopexec	lib/project1/start_stop/manual_start_exec.py	2024-07-19 15:58:51	
+/project1/ui/v_select/vD_Execute	datexec	lib/project1/ui/v_select/vD_Execute.py	2024-07-19 16:08:19	
+/project1/ui/v_select/vL_Execute	chopexec	lib/project1/ui/v_select/vL_Execute.py	2024-07-19 16:08:19	
+/project1/start_stop/tools	execute	lib/project1/start_stop/tools.py	2024-07-19 16:08:19	
+/project1/start_stop/startLTC	chopexec	lib/project1/start_stop/startLTC.py	2024-07-19 16:08:19	
+/project1/start_stop/trigExecute	chopexec	lib/project1/start_stop/trigExecute.py	2024-07-19 16:08:19	
+/project1/start_stop/manual_start_exec	chopexec	lib/project1/start_stop/manual_start_exec.py	2024-07-19 16:08:19	
