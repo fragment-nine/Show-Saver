b95ecf362c241ff260e957b8f1b38f124eb85746
ishboo <jacksonegallagher@gmail.com>
2024-08-08 17:26:45 +0800

    Trying to get everything back into one piece

--- lib/globals.py
+++ lib/globals.py
@@ -1,11 +1,11 @@
 v1l	9.0
-v1d	V1|||0x2100000534d2109|||0|||0|||USB Video
-a1l	default
-a1d	AppleUSBAudioEngine:ZOOM Corporation:AMS-24:100000:1,2
+v1d	V1|||3F45E80A-0176-46F7-B185-BB9E2C0E82E3|||0|||0|||FaceTime HD Camera
+a1l	None
+a1d	default
 a2l	default
-a2d	AppleUSBAudioEngine:MACROSILICON:USB Video:2100000:3
+a2d	BuiltInMicrophoneDevice
 a3l	default
-a3d	AppleUSBAudioEngine:ZOOM Corporation:AMS-24:100000:1,2
+a3d	BlackHole2ch_UID
 postRoll	15
-outputFolder	/Users/f9/FragmentNine Dropbox/0 F9 Team/290_HU_Japan_24/01_Working/11_Recordings
-longPrefix	HU_
+outputFolder	None
+longPrefix	HU
--- lib/project1/Embody/externalizations.py
+++ lib/project1/Embody/externalizations.py
@@ -1,9 +1,9 @@
 network_path	type	rel_file_path	timestamp	dirty
-/project1/ui	container	lib/project1/ui.tox	2024-06-19 07:29:33	False
-/project1/deko	container	lib/project1/deko.tox	2024-06-20 06:44:59	False
-/project1/start_stop	container	lib/project1/start_stop.tox	2024-07-05 17:07:37	False
-/project1/video_input	container	lib/project1/video_input.tox	2024-07-12 19:03:40	False
-/project1/track_master	container	lib/project1/track_master.tox	2024-07-06 01:14:51	False
+/project1/ui	container	lib/project1/ui.tox	2024-08-08 16:59:20	False
+/project1/deko	container	lib/project1/deko.tox	2024-08-08 16:59:20	False
+/project1/start_stop	container	lib/project1/start_stop.tox	2024-08-08 17:20:27	False
+/project1/video_input	container	lib/project1/video_input.tox	2024-08-08 17:20:27	False
+/project1/track_master	container	lib/project1/track_master.tox	2024-08-08 16:59:20	False
 /project1/Embody/externalizations	table	lib/project1/Embody/externalizations.py	2023-12-29 14:57:55	
 /project1/start_stop/startLTC	chopexec	lib/project1/start_stop/startLTC.py	2023-12-29 14:58:39	
 /globals	table	lib/globals.py	2023-12-29 15:01:36	
@@ -17,3 +17,8 @@ network_path	type	rel_file_path	timestamp	dirty
 /device_data/text1	text	lib/device_data/text1.py	2023-12-29 15:01:36	
 /device_data/text2	text	lib/device_data/text2.py	2023-12-29 15:01:36	
 /device_data/refresh_av_info	chopexec	lib/device_data/refresh_av_info.py	2023-12-29 15:01:36	
+/SS_UI_v2/UI_Main/left_data/0_record	container	lib/SS_UI_v2/UI_Main/left_data/0_record.tox	2024-08-08 17:25:34	False
+/SS_UI_v2/UI_Main/left_data/3_cc_delay	container	lib/SS_UI_v2/UI_Main/left_data/3_cc_delay.tox	2024-08-08 17:25:34	False
+/SS_UI_v2/UI_Main/left_data/1_5_pg_select	container	lib/SS_UI_v2/UI_Main/left_data/1_5_pg_select.tox	2024-08-08 17:25:34	False
+/SS_UI_v2/UI_Main/left_data/1_input_select	container	lib/SS_UI_v2/UI_Main/left_data/1_input_select.tox	2024-08-08 17:25:34	False
+/SS_UI_v2/UI_Main/left_data/2_audio_things	container	lib/SS_UI_v2/UI_Main/left_data/2_audio_things.tox	2024-08-08 17:25:34	False
