3475efc7eaad090f57084e735ef42abe386bb259
ishboo <jacksonegallagher@gmail.com>
2024-08-16 17:57:27 +0800

    End of taipei

--- lib/globals.py
+++ lib/globals.py
@@ -1,11 +1,11 @@
-v1l	9.0
-v1d	V1|||3F45E80A-0176-46F7-B185-BB9E2C0E82E3|||0|||0|||FaceTime HD Camera
+﻿v1l	9.0
+v1d	V1|||828E1AFB-A01E-4ACC-B4AE-CA9C00000001|||1|||0|||Jackson’s iPhone Camera
 a1l	None
 a1d	default
 a2l	default
-a2d	BuiltInMicrophoneDevice
+a2d	828E1AFB-A01E-4ACC-B4AE-CA9C00000003
 a3l	default
-a3d	BlackHole2ch_UID
+a3d	AppleUSBAudioEngine:C-Media Electronics Inc.:USB Audio Device:100000:2,1
 postRoll	15
 outputFolder	/Users/jacksongallagher/FragmentNine Dropbox/0 F9 Team/290_HU_Japan_24/01_Working/11_Recordings
 longPrefix	HU
--- lib/project1/Embody/externalizations.py
+++ lib/project1/Embody/externalizations.py
@@ -2,7 +2,7 @@ network_path	type	rel_file_path	timestamp	dirty
 /project1/ui	container	lib/project1/ui.tox	2024-08-08 16:59:20	False
 /project1/deko	container	lib/project1/deko.tox	2024-08-08 16:59:20	False
 /project1/start_stop	container	lib/project1/start_stop.tox	2024-08-08 17:20:27	False
-/project1/video_input	container	lib/project1/video_input.tox	2024-08-08 17:20:27	False
+/project1/video_input	container	lib/project1/video_input.tox	2024-08-10 19:34:29	False
 /project1/track_master	container	lib/project1/track_master.tox	2024-08-08 16:59:20	False
 /project1/Embody/externalizations	table	lib/project1/Embody/externalizations.py	2023-12-29 14:57:55	
 /project1/start_stop/startLTC	chopexec	lib/project1/start_stop/startLTC.py	2023-12-29 14:58:39	
