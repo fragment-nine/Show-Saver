045c78ce7428d69240d1e967166d76f322046451
ishboo <jacksonegallagher@gmail.com>
2024-08-17 16:08:35 +0800

    Fixed device assignments

--- lib/globals.py
+++ lib/globals.py
@@ -1,11 +1,11 @@
 v1l	9.0
 v1d	V1|||D1F7C29F-5CAD-4D44-B21C-F2D100000001|||1|||0|||Goose Camera
 a1l	default
-a1d	~:AMS2_Aggregate:1
+a1d	AroundLoopback_UID
 a2l	default
 a2d	BuiltInMicrophoneDevice
 a3l	default
-a3d	AroundLoopback_UID
+a3d	BlackHole2ch_UID
 postRoll	15
 outputFolder	/Users/jacksongallagher/FragmentNine Dropbox/0 F9 Team/290_HU_Japan_24/01_Working/11_Recordings
 longPrefix	HU
--- lib/project1/Embody/externalizations.py
+++ lib/project1/Embody/externalizations.py
@@ -2,7 +2,7 @@ network_path	type	rel_file_path	timestamp	dirty
 /project1/ui	container	lib/project1/ui.tox	2024-08-08 16:59:20	False
 /project1/deko	container	lib/project1/deko.tox	2024-08-08 16:59:20	False
 /project1/start_stop	container	lib/project1/start_stop.tox	2024-08-08 17:20:27	False
-/project1/video_input	container	lib/project1/video_input.tox	2024-08-10 19:34:29	False
+/project1/video_input	container	lib/project1/video_input.tox	2024-08-17 16:08:14	False
 /project1/track_master	container	lib/project1/track_master.tox	2024-08-08 16:59:20	False
 /project1/Embody/externalizations	table	lib/project1/Embody/externalizations.py	2023-12-29 14:57:55	
 /project1/start_stop/startLTC	chopexec	lib/project1/start_stop/startLTC.py	2023-12-29 14:58:39	
@@ -24,6 +24,8 @@ network_path	type	rel_file_path	timestamp	dirty
 /SS_UI_v2/UI_Main/left_data/0_record/enable_pgm	button	lib/SS_UI_v2/UI_Main/left_data/0_record/enable_pgm.tox	2024-08-17 15:38:34	False
 /SS_UI_v2/UI_Main/left_data/1_input_select/opviewer1	opviewer	lib/SS_UI_v2/UI_Main/left_data/1_input_select/opviewer1.tox	2024-08-17 15:38:34	False
 /SS_UI_v2/UI_Main/left_data/1_input_select/device_data	container	lib/SS_UI_v2/UI_Main/left_data/1_input_select/device_data.tox	2024-08-17 16:03:20	False
-/SS_UI_v2/UI_Main/left_data/1_input_select/input_select	container	lib/SS_UI_v2/UI_Main/left_data/1_input_select/input_select.tox	2024-08-17 15:38:34	False
+/SS_UI_v2/UI_Main/left_data/1_input_select/input_select	container	lib/SS_UI_v2/UI_Main/left_data/1_input_select/input_select.tox	2024-08-17 16:08:14	False
 /SS_UI_v2/UI_Main/right_feed	container	lib/SS_UI_v2/UI_Main/right_feed.tox	2024-08-17 15:38:34	False
 /device_data	container	lib/device_data.tox	2024-08-17 16:04:02	False
+/project1/files_out	container	lib/project1/files_out.tox	2024-08-17 16:08:14	False
+/project1/audio_input	container	lib/project1/audio_input.tox	2024-08-17 16:08:14	False
