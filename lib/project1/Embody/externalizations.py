a1c28640f567008e9955de16f8c697597bf924a4
ishboo <jacksonegallagher@gmail.com>
2024-08-17 16:40:06 +0800

    Started work on trackmaster

--- lib/project1/Embody/externalizations.py
+++ lib/project1/Embody/externalizations.py
@@ -3,7 +3,7 @@ network_path	type	rel_file_path	timestamp	dirty
 /project1/deko	container	lib/project1/deko.tox	2024-08-08 16:59:20	False
 /project1/start_stop	container	lib/project1/start_stop.tox	2024-08-08 17:20:27	False
 /project1/video_input	container	lib/project1/video_input.tox	2024-08-17 16:08:14	False
-/project1/track_master	container	lib/project1/track_master.tox	2024-08-08 16:59:20	False
+/project1/track_master	container	lib/project1/track_master.tox	2024-08-17 16:21:59	False
 /project1/Embody/externalizations	table	lib/project1/Embody/externalizations.py	2023-12-29 14:57:55	
 /project1/start_stop/startLTC	chopexec	lib/project1/start_stop/startLTC.py	2023-12-29 14:58:39	
 /globals	table	lib/globals.py	2023-12-29 15:01:36	
@@ -14,12 +14,12 @@ network_path	type	rel_file_path	timestamp	dirty
 /project1/start_stop/manual_start_exec	chopexec	lib/project1/start_stop/manual_start_exec.py	2023-12-29 15:01:36	
 /project1/track_master/tools	execute	lib/project1/track_master/tools.py	2023-12-29 15:01:36	
 /project1/track_master/reloadLTCExecute	chopexec	lib/project1/track_master/reloadLTCExecute.py	2023-12-29 15:01:36	
-/SS_UI_v2/UI_Main/left_data/0_record	container	lib/SS_UI_v2/UI_Main/left_data/0_record.tox	2024-08-17 15:58:03	False
+/SS_UI_v2/UI_Main/left_data/0_record	container	lib/SS_UI_v2/UI_Main/left_data/0_record.tox	1601-01-01 07:39:34	False
 /SS_UI_v2/UI_Main/left_data/3_cc_delay	container	lib/SS_UI_v2/UI_Main/left_data/3_cc_delay.tox	2024-08-08 17:25:34	False
 /SS_UI_v2/UI_Main/left_data/1_5_pg_select	container	lib/SS_UI_v2/UI_Main/left_data/1_5_pg_select.tox	2024-08-08 17:25:34	False
-/SS_UI_v2/UI_Main/left_data/1_input_select	container	lib/SS_UI_v2/UI_Main/left_data/1_input_select.tox	2024-08-08 17:27:45	False
+/SS_UI_v2/UI_Main/left_data/1_input_select	container	lib/SS_UI_v2/UI_Main/left_data/1_input_select.tox	2024-08-17 16:25:22	False
 /SS_UI_v2/UI_Main/left_data/2_audio_things	container	lib/SS_UI_v2/UI_Main/left_data/2_audio_things.tox	2024-08-08 17:25:34	False
-/SS_UI_v2/UI_Main/left_data	container	lib/SS_UI_v2/UI_Main/left_data.tox	2024-08-17 15:58:03	False
+/SS_UI_v2/UI_Main/left_data	container	lib/SS_UI_v2/UI_Main/left_data.tox	1601-01-01 07:39:34	False
 /SS_UI_v2/UI_Main/left_data/0_record/enable_tc	button	lib/SS_UI_v2/UI_Main/left_data/0_record/enable_tc.tox	2024-08-17 15:38:34	False
 /SS_UI_v2/UI_Main/left_data/0_record/enable_pgm	button	lib/SS_UI_v2/UI_Main/left_data/0_record/enable_pgm.tox	2024-08-17 15:38:34	False
 /SS_UI_v2/UI_Main/left_data/1_input_select/opviewer1	opviewer	lib/SS_UI_v2/UI_Main/left_data/1_input_select/opviewer1.tox	2024-08-17 15:38:34	False
@@ -31,3 +31,9 @@ network_path	type	rel_file_path	timestamp	dirty
 /project1/audio_input	container	lib/project1/audio_input.tox	2024-08-17 16:08:14	False
 /SS_UI_v2	container	lib/SS_UI_v2.tox	2024-08-17 16:17:49	False
 /project1	container	lib/project1.tox	2024-08-17 16:17:50	False
+/SS_UI_v2/UI_Main/left_data/1_input_select/device_data/openTrackMaster	button	lib/SS_UI_v2/UI_Main/left_data/1_input_select/device_data/openTrackMaster.tox	2024-08-17 16:24:58	False
+/SS_UI_v2/UI_Main/left_data/1_input_select/device_data/reloadTrackMaster	button	lib/SS_UI_v2/UI_Main/left_data/1_input_select/device_data/reloadTrackMaster.tox	2024-08-17 16:25:40	False
+/SS_UI_v2/UI_Main/left_data/1_input_select/device_data/fieldString	widget	lib/SS_UI_v2/UI_Main/left_data/1_input_select/device_data/fieldString.tox	2024-08-17 16:26:03	False
+/SS_UI_v2/UI_Main/left_data/1_input_select/device_data/fieldFolderBrowser	widget	lib/SS_UI_v2/UI_Main/left_data/1_input_select/device_data/fieldFolderBrowser.tox	2024-08-17 16:26:03	False
+/SS_UI_v2/UI_Main/left_data/1_input_select/device_data/datexec1	datexec	lib/SS_UI_v2/UI_Main/left_data/1_input_select/device_data/datexec1.py	2024-08-17 16:26:03	
+/SS_UI_v2/UI_Main/left_data/1_input_select/device_data/datexec2	datexec	lib/SS_UI_v2/UI_Main/left_data/1_input_select/device_data/datexec2.py	2024-08-17 16:26:03	
