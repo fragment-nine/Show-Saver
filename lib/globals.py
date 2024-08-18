912d172ea7a13819c569c03305396b539d0d0f0b
ishboo <jacksonegallagher@gmail.com>
2024-08-18 13:21:16 +0800

    Lots of fixing

--- lib/globals.py
+++ lib/globals.py
@@ -1,5 +1,5 @@
-v1l	5.0
-v1d	Academy Engraved LET
+v1l	9.0
+v1d	V1|||D1F7C29F-5CAD-4D44-B21C-F2D100000001|||1|||0|||Goose Camera
 a1l	default
 a1d	AroundLoopback_UID
 a2l	default
@@ -8,4 +8,4 @@ a3l	default
 a3d	BlackHole2ch_UID
 postRoll	15
 outputFolder	/Users/jacksongallagher/FragmentNine Dropbox/0 F9 Team/290_HU_Japan_24/01_Working/11_Recordings
-longPrefix	HU
+longPrefix	HU_
