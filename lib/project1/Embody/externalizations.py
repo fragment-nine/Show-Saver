5385287bd95a3018e24152ca84a867595636a35c
ishboo <jacksonegallagher@gmail.com>
2024-08-18 12:20:27 +0800

    Cleanup

--- lib/project1/Embody/externalizations.py
+++ lib/project1/Embody/externalizations.py
@@ -1,25 +1,24 @@
 network_path	type	rel_file_path	timestamp	dirty
-/project1/ui	container	lib/project1/ui.tox	2024-08-17 17:04:13	False
 /project1/deko	container	lib/project1/deko.tox	2024-08-17 17:04:13	False
-/project1/start_stop	container	lib/project1/start_stop.tox	2024-08-17 17:04:13	False
-/project1/video_input	container	lib/project1/video_input.tox	2024-08-17 17:04:13	False
-/project1/track_master	container	lib/project1/track_master.tox	2024-08-17 17:03:35	False
+/project1/start_stop	container	lib/project1/start_stop.tox	2024-08-18 12:20:15	False
+/project1/video_input	container	lib/project1/video_input.tox	2024-08-18 12:16:58	False
+/project1/track_master	container	lib/project1/track_master.tox	2024-08-18 12:20:15	False
 /project1/Embody/externalizations	table	lib/project1/Embody/externalizations.py	2023-12-29 14:57:55	
 /project1/start_stop/startLTC	chopexec	lib/project1/start_stop/startLTC.py	2023-12-29 14:58:39	
 /globals	table	lib/globals.py	2023-12-29 15:01:36	
-/project1/ui/v_select/vD_Execute	datexec	lib/project1/ui/v_select/vD_Execute.py	2023-12-29 15:01:36	
-/project1/ui/v_select/vL_Execute	chopexec	lib/project1/ui/v_select/vL_Execute.py	2023-12-29 15:01:36	
+/SS_UI_v2/UI_Main/left_data/1_input_select/input_select/v_select/vD_Execute	datexec	lib/SS_UI_v2/UI_Main/left_data/1_input_select/input_select/v_select/vD_Execute.py	2023-12-29 15:01:36	
+/SS_UI_v2/UI_Main/left_data/1_input_select/input_select/v_select/vL_Execute	chopexec	lib/SS_UI_v2/UI_Main/left_data/1_input_select/input_select/v_select/vL_Execute.py	2023-12-29 15:01:36	
 /project1/start_stop/tools	execute	lib/project1/start_stop/tools.py	2023-12-29 15:01:36	
 /project1/start_stop/trigExecute	chopexec	lib/project1/start_stop/trigExecute.py	2023-12-29 15:01:36	
 /project1/start_stop/manual_start_exec	chopexec	lib/project1/start_stop/manual_start_exec.py	2023-12-29 15:01:36	
 /project1/track_master/tools	execute	lib/project1/track_master/tools.py	2023-12-29 15:01:36	
 /project1/track_master/reloadLTCExecute	chopexec	lib/project1/track_master/reloadLTCExecute.py	2023-12-29 15:01:36	
-/SS_UI_v2/UI_Main/left_data/0_record	container	lib/SS_UI_v2/UI_Main/left_data/0_record.tox	2024-08-17 17:14:15	False
-/SS_UI_v2/UI_Main/left_data/3_cc_delay	container	lib/SS_UI_v2/UI_Main/left_data/3_cc_delay.tox	2024-08-17 17:03:35	False
+/SS_UI_v2/UI_Main/left_data/0_record	container	lib/SS_UI_v2/UI_Main/left_data/0_record.tox	2024-08-18 12:20:15	False
+/SS_UI_v2/UI_Main/left_data/3_cc_delay	container	lib/SS_UI_v2/UI_Main/left_data/3_cc_delay.tox	2024-08-18 12:16:58	False
 /SS_UI_v2/UI_Main/left_data/1_5_pg_select	container	lib/SS_UI_v2/UI_Main/left_data/1_5_pg_select.tox	2024-08-17 17:03:35	False
 /SS_UI_v2/UI_Main/left_data/1_input_select	container	lib/SS_UI_v2/UI_Main/left_data/1_input_select.tox	2024-08-18 12:11:44	False
 /SS_UI_v2/UI_Main/left_data/2_audio_things	container	lib/SS_UI_v2/UI_Main/left_data/2_audio_things.tox	2024-08-17 17:03:35	False
-/SS_UI_v2/UI_Main/left_data	container	lib/SS_UI_v2/UI_Main/left_data.tox	2024-08-17 17:03:35	False
+/SS_UI_v2/UI_Main/left_data	container	lib/SS_UI_v2/UI_Main/left_data.tox	2024-08-18 12:16:58	False
 /SS_UI_v2/UI_Main/left_data/0_record/enable_tc	button	lib/SS_UI_v2/UI_Main/left_data/0_record/enable_tc.tox	2024-08-17 17:14:15	False
 /SS_UI_v2/UI_Main/left_data/0_record/enable_pgm	button	lib/SS_UI_v2/UI_Main/left_data/0_record/enable_pgm.tox	2024-08-17 17:14:15	False
 /SS_UI_v2/UI_Main/left_data/1_input_select/opviewer1	opviewer	lib/SS_UI_v2/UI_Main/left_data/1_input_select/opviewer1.tox	2024-08-17 17:03:35	False
@@ -29,12 +28,11 @@ network_path	type	rel_file_path	timestamp	dirty
 /device_data	container	lib/device_data.tox	2024-08-17 17:03:35	False
 /project1/files_out	container	lib/project1/files_out.tox	2024-08-18 12:11:25	False
 /project1/audio_input	container	lib/project1/audio_input.tox	2024-08-17 17:03:35	False
-/SS_UI_v2	container	lib/SS_UI_v2.tox	2024-08-17 17:03:35	False
-/project1	container	lib/project1.tox	2024-08-17 17:03:35	False
-/SS_UI_v2/UI_Main/left_data/1_input_select/device_data/reloadTrackMaster	button	lib/SS_UI_v2/UI_Main/left_data/1_input_select/device_data/reloadTrackMaster.tox	2024-08-17 17:09:38	False
+/SS_UI_v2	container	lib/SS_UI_v2.tox	2024-08-18 12:16:58	False
+/project1	container	lib/project1.tox	2024-08-18 12:20:15	False
 /SS_UI_v2/UI_Main/left_data/1_input_select/device_data/fieldString	widget	lib/SS_UI_v2/UI_Main/left_data/1_input_select/device_data/fieldString.tox	2024-08-17 17:03:35	False
 /SS_UI_v2/UI_Main/left_data/1_input_select/device_data/fieldFolderBrowser	widget	lib/SS_UI_v2/UI_Main/left_data/1_input_select/device_data/fieldFolderBrowser.tox	2024-08-17 17:03:35	False
 /SS_UI_v2/UI_Main/left_data/1_input_select/device_data/datexec1	datexec	lib/SS_UI_v2/UI_Main/left_data/1_input_select/device_data/datexec1.py	2024-08-17 16:26:03	
 /SS_UI_v2/UI_Main/left_data/1_input_select/device_data/datexec2	datexec	lib/SS_UI_v2/UI_Main/left_data/1_input_select/device_data/datexec2.py	2024-08-17 16:26:03	
 /SS_UI_v2/UI_Main/left_data/0_track_master	container	lib/SS_UI_v2/UI_Main/left_data/0_track_master.tox	2024-08-17 17:10:29	False
-/SS_UI_v2/UI_Main/left_data/0_record_long	container	lib/SS_UI_v2/UI_Main/left_data/0_record_long.tox	2024-08-18 12:11:25	False
+/SS_UI_v2/UI_Main/left_data/0_record_long	container	lib/SS_UI_v2/UI_Main/left_data/0_record_long.tox	2024-08-18 12:16:58	False
