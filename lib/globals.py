8423f4d5900270cb9a82a8b1a742c5d6b0ccdd30
andrewgange <andrewmgange@gmail.com>
2024-08-21 11:39:25 -0500

    added button for toggle window on top + delay control from the UI
    still working on final placement / making delay control slider or visual based control

--- lib/globals.py
+++ lib/globals.py
@@ -1,11 +1,11 @@
-﻿v1l	9.0
-v1d	V1|||828E1AFB-A01E-4ACC-B4AE-CA9C00000001|||0|||0|||Jackson’s iPhone Camera
+v1l	9.0
+v1d	csv
 a1l	default
 a1d	AroundLoopback_UID
 a2l	default
-a2d	BuiltInMicrophoneDevice
+a2d	default
 a3l	default
-a3d	BlackHole2ch_UID
+a3d	default
 postRoll	15
 outputFolder	/Users/jacksongallagher/FragmentNine Dropbox/0 F9 Team/290_HU_Japan_24/01_Working/11_Recordings
 longPrefix	HU_
