c8979bc3c92b682fbb12c9adefbc0e159574c983
andrewgange <andrewmgange@gmail.com>
2024-08-26 20:37:55 -0500

    Aesthetic Overhaul for new Assets
    tidied the look of new features created in previous versions - further organizational work for new feature creation

--- lib/project1/Embody/externalizations.py
+++ lib/project1/Embody/externalizations.py
@@ -1,6 +1,6 @@
 network_path	type	rel_file_path	timestamp	dirty
 /project1	container	lib/project1.tox	2024-08-19 14:48:30	False
-/SS_UI_v2/UI_Main/left_data	container	lib/SS_UI_v2/UI_Main/left_data.tox	2024-08-26 16:38:35	False
+/SS_UI_v2/UI_Main/left_data	container	lib/SS_UI_v2/UI_Main/left_data.tox	2024-08-26 20:36:40	False
 /SS_UI_v2	container	lib/SS_UI_v2.tox	2024-08-26 16:38:35	False
 /SS_UI_v2/UI_Main/left_data/MAIN/0_record	container	lib/SS_UI_v2/UI_Main/left_data/MAIN/0_record.tox	2024-08-26 15:27:34	False
 /SS_UI_v2/UI_Main/left_data/MAIN/0_track_master	container	lib/SS_UI_v2/UI_Main/left_data/MAIN/0_track_master.tox	2024-08-26 15:27:34	False
@@ -9,10 +9,7 @@ network_path	type	rel_file_path	timestamp	dirty
 /SS_UI_v2/title/right	container	lib/SS_UI_v2/title/right.tox	2024-08-19 14:48:30	False
 /SS_UI_v2/title/left	container	lib/SS_UI_v2/title/left.tox	2024-08-19 14:48:30	False
 /SS_UI_v2/UI_Main	container	lib/SS_UI_v2/UI_Main.tox	2024-08-19 14:48:30	False
-/SS_UI_v2/UI_Main/left_data/1_input_select/device_data/fieldFolderBrowser	widget	lib/SS_UI_v2/UI_Main/left_data/1_input_select/device_data/fieldFolderBrowser.tox	2024-08-19 14:48:30	False
-/SS_UI_v2/UI_Main/left_data/3_cc_delay/delay/right_inputs/audiodevice1_delay	field	lib/SS_UI_v2/UI_Main/left_data/3_cc_delay/delay/right_inputs/audiodevice1_delay.tox	2024-08-19 14:48:30	False
-/SS_UI_v2/UI_Main/left_data/2_audio_things/viz/viz2	container	lib/SS_UI_v2/UI_Main/left_data/2_audio_things/viz/viz2.tox	2024-08-19 14:48:30	False
-/SS_UI_v2/UI_Main/left_data/MAIN/2_audio_things	container	lib/SS_UI_v2/UI_Main/left_data/MAIN/2_audio_things.tox	2024-08-26 15:27:34	False
+/SS_UI_v2/UI_Main/left_data/MAIN/2_audio_things	container	lib/SS_UI_v2/UI_Main/left_data/MAIN/2_audio_things.tox	2024-08-26 16:42:35	False
 /project1/audio_input	container	lib/project1/audio_input.tox	2024-08-19 14:48:30	False
 /SS_UI_v2/UI_Main/left_data/MAIN/1_5_pg_select1	container	lib/SS_UI_v2/UI_Main/left_data/MAIN/1_5_pg_select1.tox	2024-08-26 15:27:34	False
 /SS_UI_v2/UI_Main/left_data/MAIN/1_5_pg_select1/chopexec2	chopexec	lib/SS_UI_v2/UI_Main/left_data/MAIN/1_5_pg_select1/chopexec2.py	2024-08-18 12:49:56	
@@ -31,7 +28,7 @@ network_path	type	rel_file_path	timestamp	dirty
 /SS_UI_v2/UI_Main/left_data/MAIN/1_input_select	container	lib/SS_UI_v2/UI_Main/left_data/MAIN/1_input_select.tox	2024-08-26 16:06:43	False
 /project1/deko	container	lib/project1/deko.tox	2024-08-26 16:38:35	False
 /SS_UI_v2/UI_Main/right_feed	container	lib/SS_UI_v2/UI_Main/right_feed.tox	2024-08-19 14:48:30	False
-/SS_UI_v2/UI_Main/left_data/MAIN/1_5_pg_select	container	lib/SS_UI_v2/UI_Main/left_data/MAIN/1_5_pg_select.tox	2024-08-21 11:37:39	False
+/SS_UI_v2/UI_Main/left_data/MAIN/1_5_pg_select	container	lib/SS_UI_v2/UI_Main/left_data/MAIN/1_5_pg_select.tox	2024-08-26 16:38:36	False
 /device_data	container	lib/device_data.tox	2024-08-19 14:48:30	False
 /SS_UI_v2/UI_Main/left_data/1_input_select/device_data/datexec2	datexec	lib/SS_UI_v2/UI_Main/left_data/1_input_select/device_data/datexec2.py	2024-08-17 16:26:03	
 /SS_UI_v2/UI_Main/left_data/1_input_select/device_data/datexec1	datexec	lib/SS_UI_v2/UI_Main/left_data/1_input_select/device_data/datexec1.py	2024-08-17 16:26:03	
