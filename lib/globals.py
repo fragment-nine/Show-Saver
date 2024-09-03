c1102784c3f0ef96a988488d5ffe747e9e939f89
andrewgange <andrewmgange@gmail.com>
2024-09-03 10:16:50 -0700

    Audio Routing Mid Push

--- lib/globals.py
+++ lib/globals.py
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 v1l	0.0
 v1d	V1|||\\?\usb#vid_12d1&pid_4321&mi_00#9&1350f74c&2&0000#{65e8773d-8f56-11d0-a3b9-00a0c9223196}\global|||0|||0|||j5 WebCam JVCU100
 a1l	default
@@ -6,6 +7,16 @@ a2l	default
 a2d	{0.0.1.00000000}.{bb00c978-a7db-4fe6-b0ea-751233a5297a}||Microphone_Array_(Realtek(R)_Audio)||6
 a3l	default
 a3d	default
+=======
+v1l	14.0
+v1d	None
+a1l	None
+a1d	{0.0.1.00000000}.{93aed33f-4438-45ce-b0f9-b8a540a791ee}||Input_terminal_(Mic-j5_WebCam_JVCU100)||1
+a2l	None
+a2d	{0.0.1.00000000}.{deafa907-bbcf-4db7-93e2-26fa7779782d}||Microphone_(Yeti_Stereo_Microphone)||8
+a3l	None
+a3d	{0.0.1.00000000}.{bb00c978-a7db-4fe6-b0ea-751233a5297a}||Microphone_Array_(Realtek(R)_Audio)||6
+>>>>>>> Stashed changes
 postRoll	15
 outputFolder	/Users/jacksongallagher/FragmentNine Dropbox/0 F9 Team/290_HU_Japan_24/01_Working/11_Recordings
 longPrefix	HU_
