69779ee4a2da74249a91e440efd3434f6dccc79b
ishboo <jacksonegallagher@gmail.com>
2024-09-07 13:51:33 -0400

    Remove transform OP

--- lib/project1/Embody/externalizations.py
+++ lib/project1/Embody/externalizations.py
@@ -51,7 +51,7 @@ network_path	type	rel_file_path	timestamp	dirty
 /project1/start_stop	container	lib/project1/start_stop.tox	2024-08-26 10:30:11	False
 /SS_UI_v2/UI_Main/left_data/3_cc_delay	container	lib/SS_UI_v2/UI_Main/left_data/3_cc_delay.tox	2024-09-05 16:18:02	False
 /SS_UI_v2/UI_Main/left_data/0_record_long	container	lib/SS_UI_v2/UI_Main/left_data/0_record_long.tox	2024-09-05 16:18:02	False
-/project1/video_input	container	lib/project1/video_input.tox	2024-09-05 16:18:02	False
+/project1/video_input	container	lib/project1/video_input.tox	2024-09-07 13:51:15	False
 /SS_UI_v2/UI_Main/left_data/1_input_select	container	lib/SS_UI_v2/UI_Main/left_data/1_input_select.tox	2024-09-06 08:16:59	False
 /project1/deko	container	lib/project1/deko.tox	2024-09-05 16:18:02	False
 /SS_UI_v2/UI_Main/right_feed	container	lib/SS_UI_v2/UI_Main/right_feed.tox	2024-08-26 10:30:11	False
