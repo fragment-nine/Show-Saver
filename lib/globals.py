6c305703b89c63a5cb7f8f2dd86d72e6729afc91
ishboo <jacksonegallagher@gmail.com>
2024-09-08 10:54:45 -0400

    Fix video signal issue

--- lib/globals.py
+++ lib/globals.py
@@ -1,7 +1,7 @@
-v1l	None
-v1d	None
-a1l	None
-a1d	None
+v1l	9.0
+v1d	V1|||0x24200029350005|||0|||0|||USB Capture SDI+
+a1l	default
+a1d	com_motu_driver_coreuac_control_interface:m4ma0358t7
 a2l	None
 a2d	None
 a3l	None
@@ -10,6 +10,6 @@ postRoll	15
 outputFolder	
 longPrefix	Long
 pgm	device1_chan1
-tc	device1_chan1
+tc	device1_chan2
 pgmbu	device1_chan1
-tccompare	device1_chan1
+tccompare	device1_chan2
--- lib/project1/Embody/externalizations.py
+++ lib/project1/Embody/externalizations.py
@@ -4,7 +4,7 @@ network_path	type	rel_file_path	timestamp	dirty
 /SS_UI_v2	container	lib/SS_UI_v2.tox	2024-09-05 16:18:02	False
 /SS_UI_v2/UI_Main/left_data/0_record	container	lib/SS_UI_v2/UI_Main/left_data/0_record.tox	2024-09-05 16:18:02	False
 /SS_UI_v2/UI_Main/left_data/0_track_master	container	lib/SS_UI_v2/UI_Main/left_data/0_track_master.tox	2024-09-05 16:18:02	False
-/project1/track_master	container	lib/project1/track_master.tox	2024-08-26 10:30:11	False
+/project1/track_master	container	lib/project1/track_master.tox	2024-09-05 16:15:39	False
 /SS_UI_v2/UI_Main/left_data/1_input_select/device_data/fieldString	widget	lib/SS_UI_v2/UI_Main/left_data/1_input_select/device_data/fieldString.tox	2024-09-05 16:18:02	False
 /project1/files_out	container	lib/project1/files_out.tox	2024-09-05 16:18:02	False
 /SS_UI_v2/title/right	container	lib/SS_UI_v2/title/right.tox	2024-08-26 10:30:11	False
@@ -54,7 +54,7 @@ network_path	type	rel_file_path	timestamp	dirty
 /project1/video_input	container	lib/project1/video_input.tox	2024-09-07 13:51:15	False
 /SS_UI_v2/UI_Main/left_data/1_input_select	container	lib/SS_UI_v2/UI_Main/left_data/1_input_select.tox	2024-09-06 08:16:59	False
 /project1/deko	container	lib/project1/deko.tox	2024-09-08 06:25:56	False
-/SS_UI_v2/UI_Main/right_feed	container	lib/SS_UI_v2/UI_Main/right_feed.tox	2024-08-26 10:30:11	False
+/SS_UI_v2/UI_Main/right_feed	container	lib/SS_UI_v2/UI_Main/right_feed.tox	2024-09-08 10:53:51	False
 /SS_UI_v2/UI_Main/left_data/1_input_select/opviewer1	opviewer	lib/SS_UI_v2/UI_Main/left_data/1_input_select/opviewer1.tox	2024-09-05 16:18:02	False
 /SS_UI_v2/UI_Main/left_data/1_5_pg_select	container	lib/SS_UI_v2/UI_Main/left_data/1_5_pg_select.tox	2024-09-05 16:18:02	False
 /device_data	container	lib/device_data.tox	2024-09-05 16:18:02	False
