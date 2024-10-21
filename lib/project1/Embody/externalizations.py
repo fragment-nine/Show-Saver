7b2471d671f8de765666000fda64bf5f70833551
ishboo <jacksonegallagher@gmail.com>
2024-10-21 11:55:34 -0500

    Show full video input in UI

--- lib/project1/Embody/externalizations.py
+++ lib/project1/Embody/externalizations.py
@@ -1,17 +1,17 @@
 network_path	type	rel_file_path	timestamp	dirty
-/device_data	container	lib/device_data.tox	2024-10-21 08:28:45	False
+/device_data	container	lib/device_data.tox	2024-10-21 08:31:16	False
 /device_data/refresh_av_info	chopexec	lib/device_data/refresh_av_info.py	2024-08-20 20:38:02	
 /device_data/text1	text	lib/device_data/text1.py	2024-08-20 20:32:03	
 /globals	table	lib/globals.py	2023-12-29 15:01:36	
 /onStart	execute	lib/onStart.py	2024-08-27 05:58:33	
-/project1	container	lib/project1.tox	2024-10-21 08:29:27	False
+/project1	container	lib/project1.tox	2024-10-21 08:31:16	False
 /project1/audio_input	container	lib/project1/audio_input.tox	2024-10-21 08:10:59	False
 /project1/deko	container	lib/project1/deko.tox	2024-10-21 08:10:59	False
 /project1/deko/table1	table	lib/project1/deko/table1.csv	2024-10-20 11:48:10	
 /project1/Embody/externalizations	table	lib/project1/Embody/externalizations.py	2023-12-29 14:57:55	
 /project1/Embody/tagging_menu/button1	button	lib/project1/Embody/tagging_menu/button1.tox	2024-10-20 18:23:49	False
 /project1/files_out	container	lib/project1/files_out.tox	2024-10-21 08:10:59	False
-/project1/screenshot	container	lib/project1/screenshot.tox	2024-10-21 08:10:59	False
+/project1/screenshot	container	lib/project1/screenshot.tox	2024-10-21 08:31:16	False
 /project1/start_stop	container	lib/project1/start_stop.tox	2024-10-21 08:10:59	False
 /project1/start_stop/manual_start_exec	chopexec	lib/project1/start_stop/manual_start_exec.py	2023-12-29 15:01:36	
 /project1/start_stop/startLTC	chopexec	lib/project1/start_stop/startLTC.py	2023-12-29 14:58:39	
@@ -28,8 +28,8 @@ network_path	type	rel_file_path	timestamp	dirty
 /SS_UI_v2/title	container	lib/SS_UI_v2/title.tox	2024-10-21 08:10:59	False
 /SS_UI_v2/title/left	container	lib/SS_UI_v2/title/left.tox	2024-09-23 18:04:08	False
 /SS_UI_v2/title/right	container	lib/SS_UI_v2/title/right.tox	2024-09-23 18:04:08	False
-/SS_UI_v2/UI_Main	container	lib/SS_UI_v2/UI_Main.tox	2024-10-21 08:10:59	False
-/SS_UI_v2/UI_Main/left_data	container	lib/SS_UI_v2/UI_Main/left_data.tox	2024-10-21 08:10:59	False
+/SS_UI_v2/UI_Main	container	lib/SS_UI_v2/UI_Main.tox	2024-10-21 11:54:57	False
+/SS_UI_v2/UI_Main/left_data	container	lib/SS_UI_v2/UI_Main/left_data.tox	2024-10-21 08:31:16	False
 /SS_UI_v2/UI_Main/left_data/AUDIO	container	lib/SS_UI_v2/UI_Main/left_data/AUDIO.tox	2024-10-21 08:10:59	False
 /SS_UI_v2/UI_Main/left_data/AUDIO/audio_routing	container	lib/SS_UI_v2/UI_Main/left_data/AUDIO/audio_routing.tox	2024-10-21 08:10:59	False
 /SS_UI_v2/UI_Main/left_data/AUDIO/delay_ctrl	container	lib/SS_UI_v2/UI_Main/left_data/AUDIO/delay_ctrl.tox	2024-10-21 08:10:59	False
@@ -52,8 +52,8 @@ network_path	type	rel_file_path	timestamp	dirty
 /SS_UI_v2/UI_Main/left_data/chopexec1	chopexec	lib/SS_UI_v2/UI_Main/left_data/chopexec1.py	2024-10-19 15:09:34	
 /SS_UI_v2/UI_Main/left_data/FILE	container	lib/SS_UI_v2/UI_Main/left_data/FILE.tox	2024-10-21 08:10:59	False
 /SS_UI_v2/UI_Main/left_data/LAYOUT	container	lib/SS_UI_v2/UI_Main/left_data/LAYOUT.tox	2024-10-21 08:10:59	False
-/SS_UI_v2/UI_Main/left_data/MAIN	container	lib/SS_UI_v2/UI_Main/left_data/MAIN.tox	2024-10-21 08:10:59	False
-/SS_UI_v2/UI_Main/left_data/MAIN/0_record	container	lib/SS_UI_v2/UI_Main/left_data/MAIN/0_record.tox	2024-10-21 08:10:59	False
+/SS_UI_v2/UI_Main/left_data/MAIN	container	lib/SS_UI_v2/UI_Main/left_data/MAIN.tox	2024-10-21 08:31:16	False
+/SS_UI_v2/UI_Main/left_data/MAIN/0_record	container	lib/SS_UI_v2/UI_Main/left_data/MAIN/0_record.tox	2024-10-21 08:31:16	False
 /SS_UI_v2/UI_Main/left_data/MAIN/1_input_select	container	lib/SS_UI_v2/UI_Main/left_data/MAIN/1_input_select.tox	2024-10-21 08:10:59	False
 /SS_UI_v2/UI_Main/left_data/MAIN/1_input_select/device_data	container	lib/SS_UI_v2/UI_Main/left_data/MAIN/1_input_select/device_data.tox	2024-10-21 08:10:59	False
 /SS_UI_v2/UI_Main/left_data/MAIN/1_input_select/input_select	container	lib/SS_UI_v2/UI_Main/left_data/MAIN/1_input_select/input_select.tox	2024-10-21 08:10:59	False
@@ -79,15 +79,15 @@ network_path	type	rel_file_path	timestamp	dirty
 /SS_UI_v2/UI_Main/left_data/MAIN/1_input_select/input_select/v_select/vD_Execute	datexec	lib/SS_UI_v2/UI_Main/left_data/MAIN/1_input_select/input_select/v_select/vD_Execute.py	2024-10-19 15:54:01	
 /SS_UI_v2/UI_Main/left_data/MAIN/1_input_select/input_select/v_select/vL_Execute1	datexec	lib/SS_UI_v2/UI_Main/left_data/MAIN/1_input_select/input_select/v_select/vL_Execute1.py	2024-10-20 10:37:06	
 /SS_UI_v2/UI_Main/left_data/MAIN/1_input_select/opviewer1	opviewer	lib/SS_UI_v2/UI_Main/left_data/MAIN/1_input_select/opviewer1.tox	2024-10-21 08:10:59	False
-/SS_UI_v2/UI_Main/left_data/MAIN/action_button	container	lib/SS_UI_v2/UI_Main/left_data/MAIN/action_button.tox	2024-10-21 08:29:51	False
+/SS_UI_v2/UI_Main/left_data/MAIN/action_button	container	lib/SS_UI_v2/UI_Main/left_data/MAIN/action_button.tox	2024-10-21 08:31:16	False
 /SS_UI_v2/UI_Main/left_data/view_select	widget	lib/SS_UI_v2/UI_Main/left_data/view_select.tox	2024-10-21 08:10:59	False
 /SS_UI_v2/UI_Main/left_data/viewer	select	lib/SS_UI_v2/UI_Main/left_data/viewer.tox	2024-10-21 08:10:59	False
 /SS_UI_v2/UI_Main/right_feed	container	lib/SS_UI_v2/UI_Main/right_feed.tox	2024-10-21 08:10:59	False
-/SS_UI_v2/UI_Main/left_data/MAIN/3_cc_delay/select1	select	lib/SS_UI_v2/UI_Main/left_data/MAIN/3_cc_delay/select1.tox	2024-10-21 08:26:52	False
-/SS_UI_v2/UI_Main/left_data/MAIN/3_cc_delay/tc_compare	container	lib/SS_UI_v2/UI_Main/left_data/MAIN/3_cc_delay/tc_compare.tox	2024-10-21 08:26:52	False
-/SS_UI_v2/UI_Main/left_data/MAIN/3_cc_delay/color_correct	container	lib/SS_UI_v2/UI_Main/left_data/MAIN/3_cc_delay/color_correct.tox	2024-10-21 08:26:52	False
-/SS_UI_v2/UI_Main/left_data/MAIN/3_cc_delay/color_correct/TOP	container	lib/SS_UI_v2/UI_Main/left_data/MAIN/3_cc_delay/color_correct/TOP.tox	2024-10-21 08:25:44	False
-/SS_UI_v2/UI_Main/left_data/MAIN/3_cc_delay/color_correct/container1	container	lib/SS_UI_v2/UI_Main/left_data/MAIN/3_cc_delay/color_correct/container1.tox	2024-10-21 08:26:52	False
+/SS_UI_v2/UI_Main/left_data/MAIN/3_cc_delay/select1	select	lib/SS_UI_v2/UI_Main/left_data/MAIN/3_cc_delay/select1.tox	2024-10-21 08:31:16	False
+/SS_UI_v2/UI_Main/left_data/MAIN/3_cc_delay/tc_compare	container	lib/SS_UI_v2/UI_Main/left_data/MAIN/3_cc_delay/tc_compare.tox	2024-10-21 08:31:16	False
+/SS_UI_v2/UI_Main/left_data/MAIN/3_cc_delay/color_correct	container	lib/SS_UI_v2/UI_Main/left_data/MAIN/3_cc_delay/color_correct.tox	2024-10-21 08:31:16	False
+/SS_UI_v2/UI_Main/left_data/MAIN/3_cc_delay/color_correct/TOP	container	lib/SS_UI_v2/UI_Main/left_data/MAIN/3_cc_delay/color_correct/TOP.tox	2024-10-21 08:31:16	False
+/SS_UI_v2/UI_Main/left_data/MAIN/3_cc_delay/color_correct/container1	container	lib/SS_UI_v2/UI_Main/left_data/MAIN/3_cc_delay/color_correct/container1.tox	2024-10-21 08:31:16	False
 /SS_UI_v2/UI_Main/left_data/MAIN/3_cc_delay/color_correct/TOP	container	lib/SS_UI_v2/UI_Main/left_data/MAIN/3_cc_delay/color_correct/TOP.tox	1600-12-31 18:12:16	False
 /SS_UI_v2/UI_Main/left_data/MAIN/3_cc_delay/color_correct/container1	container	lib/SS_UI_v2/UI_Main/left_data/MAIN/3_cc_delay/color_correct/container1.tox	1600-12-31 18:12:16	False
 /SS_UI_v2/UI_Main/left_data/MAIN/3_cc_delay/select1	select	lib/SS_UI_v2/UI_Main/left_data/MAIN/3_cc_delay/select1.tox	1600-12-31 18:12:16	False
